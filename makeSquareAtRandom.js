b736b14d36ee5bb7b05d39c792e6b80d8ba212c9
negibose2020 <allium.bose@gmail.com>
2020-09-20 23:46:45 +0900

    1st commit

---
+++ makeSquareAtRandom.js
@@ -0,0 +1,32 @@
+function makeManySquares(num = 10, len = 30) { //描画する数と1辺の長さは可変。未指定時は、数は10、1辺の長さは30
+    for (let i = 0; i < num; i++) {
+        makeSquareAtRandom(len)
+    }
+}
+
+function makeSquareAtRandom(len = 30) { //1辺の長さは可変。未指定時は30
+    var canvas = document.getElementById("canvas");
+    if (canvas.getContext) {
+        let ctx = canvas.getContext("2d");
+        let obtainedColor = randomColorCode();
+        let lengthOfSquare = len;
+        let randomWidth = Math.floor((canvas.width - lengthOfSquare) * Math.random());
+        let randomHeight = Math.floor((canvas.height - lengthOfSquare) * Math.random());
+
+        ctx.fillStyle = obtainedColor;
+        ctx.fillRect(randomWidth, randomHeight, lengthOfSquare, lengthOfSquare);
+    }
+}
+
+function randomColorCode() {
+    let randomColor = "#";
+    for (let i = 0; i < 6; i++) {
+        randomColor += generateHexCharacter();
+    }
+
+    function generateHexCharacter() {
+        return Math.floor(16 * Math.random()).toString(16);
+    }
+
+    return randomColor;
+}
