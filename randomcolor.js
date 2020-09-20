84dbcc93f139eab8a12cce4b313a474d35b18f15
negibose2020 <allium.bose@gmail.com>
2020-09-20 20:52:47 +0900

    Fixed spelling.

--- randomcolor.js
+++ randomcolor.js
@@ -1,7 +1,7 @@
 function randomColorCode() {
     var randomColor = '#'
     for (i = 0; i < 6; i++) {
-        randomColor += generateHexCharactor()
+        randomColor += generateHexCharacter()
     }
 
     function generateHexCharacter() {
