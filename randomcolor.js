dafef46dd3bc0e7b71767ecc1f668ced46e1fcde
negibose2020 <allium.bose@gmail.com>
2020-09-20 20:15:02 +0900

    add function generateHexCharactor

--- randomcolor.js
+++ randomcolor.js
@@ -1,7 +1,12 @@
-function randomColorCode(){
+function randomColorCode() {
     var randomColor = '#'
-    for (i=0;i<6;i++){
-        randomColor+=generateHexCharactor()
+    for (i = 0; i < 6; i++) {
+        randomColor += generateHexCharactor()
     }
 
+    function generateHexCharacter() {
+        return Math.floor(16 * Math.random()).toString(16)
+    }
+
+    return randomColor
 }
