a5b7c7800c0a9f303c6ace9067db70694c62b372
negibose2020 <allium.bose@gmail.com>
2020-09-22 22:26:28 +0900

    setInterval関連の関数を追加。この関数をインスタンスとすることで、clearIntervalの制御も可能。複数のインスタンスを作成し、制御することも可能。

--- makeSquareAtRandom.js
+++ makeSquareAtRandom.js
@@ -1,17 +1,43 @@
-function makeManySquares(num = 10, len = 30) { //描画する数と1辺の長さは可変。未指定時は、数は10、1辺の長さは30
+function DrawSquares(num = 10, len = 30) {
+    this.intervalId = null;
+    this.stop = () => {
+        if (this.intervalId != null) {
+            clearInterval(this.intervalId);
+            this.intervalId = null;
+        } else {
+            console.log("The process hasn't started yet.");
+        }
+    };
+    this.draw = () => {
+        if (this.intervalId != null) {
+            console.log("Drawing!!");
+            return;
+        } else {
+            this.intervalId = setInterval(makeManySquares, 500, num, len);
+        }
+    };
+}
+
+function makeManySquares(num = 10, len = 30) {
+    //描画する数と1辺の長さは可変。未指定時は、数は10、1辺の長さは30
     for (let i = 0; i < num; i++) {
-        makeSquareAtRandom(len)
+        makeSquareAtRandom(len);
     }
 }
 
-function makeSquareAtRandom(len = 30) { //1辺の長さは可変。未指定時は30
+function makeSquareAtRandom(len = 30) {
+    //1辺の長さは可変。未指定時は30
     var canvas = document.getElementById("canvas");
     if (canvas.getContext) {
         let ctx = canvas.getContext("2d");
         let obtainedColor = randomColorCode();
         let lengthOfSquare = len;
-        let randomWidth = Math.floor((canvas.width - lengthOfSquare) * Math.random());
-        let randomHeight = Math.floor((canvas.height - lengthOfSquare) * Math.random());
+        let randomWidth = Math.floor(
+            (canvas.width - lengthOfSquare) * Math.random()
+        );
+        let randomHeight = Math.floor(
+            (canvas.height - lengthOfSquare) * Math.random()
+        );
 
         ctx.fillStyle = obtainedColor;
         ctx.fillRect(randomWidth, randomHeight, lengthOfSquare, lengthOfSquare);
